49eaa3dd4179f46eb84414a3c9eb5348c06ab5c6
Opgaming1 <91045339+Opgaming1@users.noreply.github.com>
2023-11-05 18:40:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+ canvas=document.getElementById("myCanvas");
+ ctx=canvas.getContext("2d");
+ color="aqua";
+ mouseevent="";
+ last_x_position="";
+ last_y_position="";
+ 
+ window.addEventListener("mousedown",my_mousedown);
+ function my_mousedown(e){
+    mouseevent="mousedown";
+ }
+
+ window.addEventListener("mouseup",my_mouseup);
+ function my_mouseup(e){
+    mouseevent="mouseup";
+ }
+ 
+ window.addEventListener("mouseleave",my_mouseleave);
+ function my_mouseleave(e){
+    mouseevent="mouseleave";
+ }
+
+ window.addEventListener("mousemove",my_mousemove);
+ function my_mousemove(e){
+    current_x_position=e.clientX - canvas.offsetLeft;
+    current_y_position=e.clientY - canvas.offsetTop;
+    if(mouseevent =="mousedown"){
+         ctx.beginPath();
+         ctx.strokeStyle=color;
+         ctx.lineWidth=2;
+         ctx.moveTo(last_x_position,last_y_position);
+         ctx.lineTo(current_x_position,current_y_position);
+         ctx.stroke();
+    }
+    last_x_position=current_x_position;
+    last_y_position=current_y_position;
+ }
